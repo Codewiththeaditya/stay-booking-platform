b17943d5d2a0cf0488aac5aaec33c04b7ea01bd9
Codewiththeaditya <apratapsingh778@gmail.com>
2025-08-24 03:21:21 +0530

    added dotenv

--- init/index.js
+++ init/index.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js")
+require("dotenv").config({path: __dirname + "/../.env"});
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = process.env.MONGO_URL;//|| "mongodb://127.0.0.1:27017/wanderlust";
 
 main().catch((err)=>{console.log(err)});
 
